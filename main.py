03e86a505d64d80be8bbee8aaf6c2f95f04ad43b
steven-halla <69405488+steven-halla@users.noreply.github.com>
2021-12-18 23:44:14 -0800

    "setting up pygame project "

--- main.py
+++ main.py
@@ -4,3 +4,8 @@ pygame.init()
 
 screen = pygame.display.set_mode((800, 600))
 
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.tyype == pygame.quit():
+            running = False
