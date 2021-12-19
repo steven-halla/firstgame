caac1a08719edf08898d40b4dc48f36d94b4842e
steven-halla <69405488+steven-halla@users.noreply.github.com>
2021-12-19 00:03:47 -0800

    "setting up pygame project "

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ pygame.init()
 
 screen = pygame.display.set_mode((800, 600))
 
+pygame.display.set_caption("first game")
+icon = pygame.image.load('ufo.png')
+pygame.display.set_icon(icon)
+
 running = True
 while running:
     for event in pygame.event.get():
