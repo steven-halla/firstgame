01c8e4d1f067bef7ccb1b5d6d4a36ddf38df746d
steven-halla <69405488+steven-halla@users.noreply.github.com>
2021-12-19 00:32:49 -0800

    "image is now displaying "

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 import pygame
 
 pygame.init()
-
 screen = pygame.display.set_mode((800, 600))
 
 pygame.display.set_caption("first game")
 icon = pygame.image.load('ufo.png')
 pygame.display.set_icon(icon)
+
+playerImg = pygame.image.load('player.png')
+playerX = 370
+playerY = 480
+
+def player():
+    screen.blit(playerImg, (playerX, playerY))
 
 running = True
 while running:
@@ -15,3 +21,6 @@ while running:
             running = False
 
     screen.fill((0, 0, 0))
+
+    player()
+    pygame.display.update()
