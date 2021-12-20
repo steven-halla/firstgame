729d15cb0c4c711313b40318a7282e79fe719bf4
steven-halla <69405488+steven-halla@users.noreply.github.com>
2021-12-19 16:42:26 -0800

    " Coding examples for movement velocity/acceleration "

--- main.py
+++ main.py
@@ -1,5 +1,48 @@
 import pygame
 
+# position (x, y)
+# velocity (x, y)
+# acceleration (x, y)
+
+# max_velocity_walk = 16
+# max_velocity_run = 32
+# acceleration_walk = 4
+# acceleration_run = 8
+
+
+# is_walking = is_key_pressed(DPAD_LEFT) || is_key_pressed(DPAD_RIGHT)
+# is_running = is_key_pressed(RUN_BUTTON) && is_walking
+
+
+# set acceleration based on walking vs running
+# if is_running:
+#     acceleration.x = acceleration_run
+# elif is_walking:
+#     acceleration.x = acceleration_walk
+# else:
+#     acceleration.x = 0
+
+
+# finally add acceleration to velocity.
+# velocity.x = velocity.x + acceleration.x
+
+
+# we've added the acceleration to velocity, now lets make sure velocity stays
+# within range of -max velocity and max velocity. different based on walking vs run
+# if is_running:
+#     if velocity.x > max_velocity_run:
+#         velocity.x = max_velocity_run
+#     elif velocity.x < -max_velocity_run:
+#         velocity.x = -max_velocity_run
+# elif is_walking:
+#     if velocity.x > max_velocity_walk:
+#         velocity.x = max_velocity_walk
+#     elif velocity.x < -max_velocity_walk:
+#         velocity.x = -max_velocity_walk
+
+# position.x = position.x + velocity.x
+
+
 pygame.init()
 screen = pygame.display.set_mode((800, 600))
 
@@ -19,7 +62,7 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
+    #RGB
     screen.fill((0, 0, 0))
 
     player()
