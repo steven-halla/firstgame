8a636d0f4fdb3e5b61d28a2e73be1424e64bda3f
steven-halla <69405488+steven-halla@users.noreply.github.com>
2021-12-19 17:52:23 -0800

    " working on ship movement on the X axis "

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import pygame
 
 
-
-
 pygame.init()
 screen = pygame.display.set_mode((800, 600))
 
@@ -13,29 +11,33 @@ pygame.display.set_icon(icon)
 playerImg = pygame.image.load('player.png')
 playerX = 370
 playerY = 480
+playerX_Change = 0
 
 def player(x,y):
     screen.blit(playerImg, (x, y))
 
+
 running = True
+
+# RGB
+screen.fill((0, 0, 0))
+
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    #RGB
-    screen.fill((0, 0, 0))
 
-    if event.type == pygame.KEYDOWN:
-        if event.key == pygame.K_LEFT:
-            print("left arrow is pressed")
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                playerX_Change = -.05
 
-        if event.key == pygame.K_RIGHT:
-            print("right arrow is pressed")
+            if event.key == pygame.K_RIGHT:
+                playerX_Change = .05
 
-    if event.type == pygame.KEYUP:
-        if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
-            print("key is no longer being pressed")
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                playerX_Change = 0
 
-
+    playerX += playerX_Change
     player(playerX, playerY)
     pygame.display.update()
